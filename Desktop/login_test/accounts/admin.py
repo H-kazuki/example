b7d794cedfd948776d5fa52ca729086b524eb54d
H-kazuki <lobqhk9869@yahoo.co.jp>
2021-01-09 09:16:35 +0900

    1/9commit

--- Desktop/login_test/accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User
-from .models import Todo
-
-class UserAdmin(admin.ModelAdmin):
-	list_display = ['username', 'email', 'password']
-
-admin.site.register(User, UserAdmin)
-admin.site.register(Todo)
